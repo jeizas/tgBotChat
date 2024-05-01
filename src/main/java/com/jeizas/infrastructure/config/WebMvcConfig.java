bae72435e16e37fdfe62782ad1bb5405c2d4e49a
zhangpj <zhangpj@t3go.cn>
2024-05-01 15:56:12 +0800

    服务器无法加载

--- src/main/java/com/jeizas/infrastructure/config/WebMvcConfig.java
+++ src/main/java/com/jeizas/infrastructure/config/WebMvcConfig.java
@@ -1,7 +1,7 @@
 package com.jeizas.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -9,17 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    /**
-     * Add resource handlers.
-     *
-     * @param registry the registry
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
 
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedHeaders("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
+                .maxAge(3600);
     }
 }
