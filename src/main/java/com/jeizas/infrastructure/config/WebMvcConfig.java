7a9294d4bfce7a72431a7273406cd310f001f24d
zhangpj <zhangpj@t3go.cn>
2024-05-01 15:52:24 +0800

    服务器无法加载

---
+++ src/main/java/com/jeizas/infrastructure/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.jeizas.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * The type Web mvc config.
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    /**
+     * Add resource handlers.
+     *
+     * @param registry the registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
